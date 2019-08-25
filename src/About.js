522d74a7fd7265d8cdf08946d258ef359f4498e1
Tovstogray Ilia <foreign.tubers@gmail.com>
2019-08-25 14:18:59 +0600

    Webpage Body

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => (
+  <div>
+    <h2>About Page</h2>
+    <p>My name is Tovstogray Ilia.</p>
+        <p>I'm a student of OmSU</p>
+        <p>This is my personal website</p>
+  </div>
+)
---
+++ src/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Contact = () => (
+  <div>
+    <h2>Contact me</h2>
+    <p>github/tovstogray</p>
+    <p>vk/tovstogray</p>
+    <p>ig/yala_ngt</p>
+  </div>
+)
